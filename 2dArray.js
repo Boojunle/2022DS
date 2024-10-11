f532d3dfb626e6255b1454f3dad8e2d9a7b551ba
Boojunle <boojunle22@gmail.com>
2024-10-11 15:52:37 +0800

    1011-5

---
+++ 2dArray.js
@@ -0,0 +1,26 @@
+var ary2d=[
+    [1,2,3], //index 0
+    [4,5,6], //index 1
+    [7,8,9]  //index 2
+];
+var Ans=[0,1,2,3,4,5,6,7,8,9];
+ //get index 0 at Ans:   Ans[0]
+ //get 6 in ary2d?
+//  ary2d[1].push("N");
+//  [
+//     [1,2,3], //index 0
+//     [4,5,6,"N"], //index 1
+//     [7,8,9]  //index 2
+
+// ];
+//  ary2d.push("N")
+//  [
+//     [1,2,3], //index 0
+//     [4,5,6], //index 1
+//     [7,8,9],  //index 2
+//     "N"
+// ];
+
+//push ["A","B","C"] to ary2d?
+ ary2d.push(["A","B","C"]);
+ console.log(ary2d);
