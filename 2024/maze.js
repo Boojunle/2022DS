4bfebcdec392adda0e9b6eda8cde52e8c0dd4ec4
Boojunle <boojunle22@gmail.com>
2024-11-29 15:02:28 +0800

    1129new

--- 2024/maze.js
+++ 2024/maze.js
@@ -1,107 +1,87 @@
-// var MAZE = [
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
-//     [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-// ];//1,1  end:8,10
+var MAZE = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]; //1,1  end:8,10
 
-MAZE=[
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,0,1],
-    [1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1],
-    [1,0,1,0,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1],
-    [1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,1,0,0,0,1],
-    [1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1],
-    [1,0,1,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1],
-    [1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1],
-    [1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,1],
-    [1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,1],
-    [1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1],
-    [1,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1],
-    [1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1],
-    [1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
-    //end:20,20
-    
-    //object oriented
-    class Point{
-      constructor(_row, _col){
+class Point {
+    constructor(_row, _col) {
         this.row = _row;
         this.col = _col;
-      }
-      isEnd = function(){
-        return this.row==end.row && this.col==end.col
-      }
     }
-    var start= new Point(1,1);//{row:1,col:1}
-    var end = new Point(19,19);//[8,10]
-    var Stack=[];
-    var step = start;
-    var rollBack=false;
-    function go(){
-         Stack.push(step);
-         
-         while(! step.isEnd()){
-            MAZE[step.row][step.col]=2;
-            
-            //up
-            if(MAZE[step.row-1][step.col] == 0)
-            {
-               if(rollBack){
-                Stack.push(step);
+    isEnd() {
+        return this.row === end.row && this.col === end.col;
+    }
+}
+
+var start = new Point(1, 1);
+var end = new Point(8, 10);
+var Stack = [];
+var step = start;
+var rollBack=false;
+
+var ary=[1,5,6,2,9];
+ary.sort((a,b)=>{return b-a})
+ary.sort((a,b)=>{return Math.random()-0.5});
+
+var dir=[{x:-1,y:0}, {x:1,y:0},{x:0,y:-1},{x:0,y:1}];
+dir.sort((a,b)=>{return b.x-a.x});
+dir.sort((a,b)=>{return Math.random()-0.5});
+
+function go() {
+    Stack.push(step);
+    while (!step.isEnd()) {
+        
+        MAZE[step.row][step.col] = 2; 
+        if (MAZE[step.row + dir[0].x][step.col+dir[0].y] == 0) { // 如果步數上方為0
+            if(rollBack){
+                Stack.push(step)
                 rollBack=false;
-               }
-                step = new Point(step.row-1, step.col);
-                Stack.push(step);
-                
-            }else if(MAZE[step.row+1][step.col] == 0){ //down
-              if(rollBack){
-                Stack.push(step);
-                rollBack=false;
-               }
-                
-               step = new Point(step.row+1, step.col);  
-                Stack.push(step);
-            }else if(MAZE[step.row][step.col-1] == 0){//left
-              if(rollBack){
-                Stack.push(step);
-                rollBack=false;
-               }
-                step = new Point(step.row, step.col-1); 
-                Stack.push(step);
-            }else if(MAZE[step.row][step.col+1] == 0){//right
-              if(rollBack){
-                Stack.push(step);
-                rollBack=false;
-               }
-                step = new Point(step.row, step.col+1);
-                Stack.push(step);
-            }else{
-                if(Stack.length>0){
-                  step = Stack.pop();
-                  rollBack=true;
-                }
-                else
-                  break;
             }
-         }
-         if(Stack.length>0)
-            console.log("Done!");
-         else 
-            console.log("No solution!");
+            step = new Point(step.row + dir[0].x, step.col + dir[0].y);
+            Stack.push(step);
+        } else if (MAZE[step.row + dir[1].x][step.col+dir[1].y] == 0) { // 如果步數下方為0
+            if(rollBack){
+                Stack.push(step)
+                rollBack=false;
+            }
+            step = new Point(step.row + dir[1].x, step.col + dir[1].y);
+            Stack.push(step);
+            
+        } else if (MAZE[step.row + dir[2].x][step.col+dir[2].y] == 0) { // 
+            if(rollBack){
+                Stack.push(step)
+                rollBack=false;
+            }
+            step = new Point(step.row + dir[2].x, step.col + dir[2].y);
+            Stack.push(step);
+            
+        } else if (MAZE[step.row + dir[3].x][step.col+dir[3].y] == 0) { // 
+            if(rollBack){
+                Stack.push(step)
+                rollBack=false;
+            }
+            step = new Point(step.row + dir[3].x, step.col + dir[3].y);
+            Stack.push(step);
+        } else // 如果四個方向都無法前進
+            if(Stack.length>0){
+               step = Stack.pop();
+               rollBack=true;
+            }
+            else
+               break;
     }
-    
-    go()
-     
+    if(Stack.length==0)
+       console.log("No solution!");
+    else
+    console.log("Done!");
+}
+
+go();
